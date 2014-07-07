9e9874003af07a38f7101d73693024e3feaa1334
root <root@localhost.localdomain>
2014-07-07 16:16:43 -0500

    Actualizando ecuaciones.py y agregando nuevo módulo imprimir_ecuaciones.py

--- imprimir_ecuaciones.py
+++ imprimir_ecuaciones.py
@@ -1,43 +1,8 @@
-from imprimir_ecuaciones import impecu
-
-class Ecuaciones:
-	"""Esta aplicacion crea una funcion polinomial para aplicar distintas operaciones algebraicas y de calculo.
-		Basta con introducir dentro de la inicializacion el orden de la ecuacion como primer parametro y como segundo
-		parametro una lista con sus respectivas constantes."""
-	def __init__(self, grado, constantes):
-		"""Contructor de la clase 'Ecuaciones'. Es imperativo indicar el grado y las constantes. El siguiente es un ejemplo
-			la declaracion de un objeto perteneciente a 'Ecuaciones':
-			
-				ecuacion = Ecuacion(grado, [constantes])"""
-		self.grado = grado  # El atributo 'grados' es guardado.
-		matriz = [0] *  (grado + 1)
-		matriz[:len(constantes)] = constantes[:] # Se crea la lista de constantes.
-		self.constantes = matriz  # El atributo 'contantes' es guardado.
-		impecu(grado, matriz)
-		
-	def derivada(self):
-		g = self.grado
-		necu = []
-		for n in self.constantes:
-			aux = n * g
-			necu.append(aux)
-			g -= 1
-		impecu(self.grado - 1, necu)
-		
-	def integral(self):
-		g = self.grado
-		necu = []
-		for n in self.constantes:
-			aux = n / (g + 1.0)
-			necu.append(aux)
-			g -= 1
-		impecu(self.grado + 1, necu)
-		
-	def impecuacion(self):
+def impecu(grado, const):
 		ecu = ''
-		g = self.grado
+		g = grado
 		aux = 0
-		for num in self.constantes:   # Bucle que imprime la ecuacion en representacion algebraica.
+		for num in const:   # Bucle que imprime la ecuacion en representacion algebraica.
 			if aux == 0:
 				aux += 1
 				if num  == 0:
@@ -133,5 +98,4 @@ class Ecuaciones:
 					else:
 						ecu = ecu + '+ ' + str(num) + 'x^' + str(g)
 						g -= 1
-		print ecu 
-
+		print ecu
